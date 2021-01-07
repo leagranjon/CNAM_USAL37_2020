67dbac1d3c6b7009ee0a2ee556fe7c216a068690
leagranjon <73827930+leagranjon@users.noreply.github.com>
2021-01-07 14:51:20 +0100

    add exercices

---
+++ TP_Freelance/tp_freelancer_init.sql
@@ -0,0 +1,51 @@
+DROP DATABASE IF EXISTS tp_freelancer;
+
+CREATE DATABASE tp_freelancer DEFAULT CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_unicode_ci';
+
+USE tp_freelancer;
+
+CREATE TABLE quotes
+(
+	quote_id INT PRIMARY KEY AUTO_INCREMENT,
+    quote_state CHAR(10) NOT NULL,
+    quote_date DATE NOT NULL,
+    quote_amount INT NOT NULL,
+    quote_final_date DATE NOT NULL,
+    quote_final_amount INT,
+);
+
+CREATE TABLE jobs
+(
+	job_id INT PRIMARY KEY AUTO_INCREMENT,
+    job_state CHAR(10) NOT NULL,
+    job_title VARCHAR(100) NOT NULL,
+    job_start DATETIME NOT NULL,
+    job_end DATETIME NOT NULL,
+    job_description TEXT NOT NULL,
+);
+
+CREATE TABLE customers
+(
+	customer_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_name VARCHAR(100) NOT NULL,
+    customer_email VARCHAR(255) NOT NULL,
+);
+
+CREATE TABLE customers_cats
+(
+	cat_id INT PRIMARY KEY AUTO_INCREMENT,
+    cat_name VARCHAR(50) NOT NULL,
+    cat_description TEXT NOT NULL,
+);
+
+ALTER TABLE customers 
+ADD 
+	CONSTRAINT fk_customers_customers_cats FOREIGN KEY (cat_id) REFERENCES customers_cats (cat_id);
+
+ALTER TABLE jobs
+ADD 
+	CONSTRAINT fk_jobs_quotes FOREIGN KEY (quote_id) REFERENCES quotes(quote_id);
+    
+ALTER TABLE customers
+ADD
+		CONSTRAINT fk_customers_jobs FOREIGN KEY (job_id) REFERENCES jobs (job_id);
